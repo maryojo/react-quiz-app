e02714fc04e0d1aa08438e4721795c165054649b
maryojo <maryojo3@gmail.com>
2022-03-05 17:04:29 +0100

    first commit

---
+++ build/precache-manifest.3cf22ca879bb329adc579f5aad70b30b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465f622b64529f7cb4b444085bf2ff53",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "a13c78a1249c9f4c3fdc",
+    "url": "/react-quiz-app/static/css/main.5a032b4e.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/react-quiz-app/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13c78a1249c9f4c3fdc",
+    "url": "/react-quiz-app/static/js/main.9d24e3e0.chunk.js"
+  },
+  {
+    "revision": "636b6fa609f4d9dd2fa2",
+    "url": "/react-quiz-app/static/js/runtime-main.4c319d39.js"
+  }
+]);
